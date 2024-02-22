e79e10814af217c046f55313b23b5030ae5c3225
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-22 18:41:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function g(id) {
+  return document.getElementById(id)
+}
+function reload() {
+  let data = JSON.parse(localStorage.getItem('todo')) || [];
+  g('preview').innerHTML = data.map(r => `<div class="card">
+  <input type="checkbox" onchange="del(${r.id})"> ${r.title}<br>${r.desc}</div>`).join('')
+}
+function del(id) {
+  let data = JSON.parse(localStorage.getItem('todo')) || [];
+  data = data.filter(e => e.id != id);
+  localStorage.setItem('todo', JSON.stringify(data))
+  reload()
+}
+function add() {
+  let data = JSON.parse(localStorage.getItem('todo')) || [];
+  data.push({
+    title: g('title').value,
+    desc: g('desc').value,
+    id: Math.floor(Math.random()*1000000)
+  })
+  g('title').value = '';
+  g('desc').value = '';
+  localStorage.setItem('todo', JSON.stringify(data))
+  reload()
+}
+reload()
