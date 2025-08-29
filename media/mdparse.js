f0c118025602be923940688f85239d3387a4ba5c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-08-29 21:47:41 +0200

    More markdown

--- media/mdparse.js
+++ media/mdparse.js
@@ -1,3 +1,4 @@
+// MDParse, Copyright Fsh-org
 window.MDParse = function(text, custom=(t)=>{return t}) {
   // Reserve
   let reserve = {};
@@ -19,6 +20,10 @@ window.MDParse = function(text, custom=(t)=>{return t}) {
         .replaceAll('~quot;', '&quot;');
       return reservemd(`<code class="block">${match.slice(3,-3)}</code>`);
     })
+    .replaceAll(/\[(.+?)\]\((~lt;https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)>|https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))\)/g, function(match,g1,g2){
+      if (match.match(/^~lt;.+?>$/m)) match=match.slice(4,-1);
+      return reservemd(`<a href="${g2}">${g1}</a>`);
+    })
     .replaceAll(/(~lt;https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)>|https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g, function(match){
       if (match.match(/^~lt;.+?>$/m)) match=match.slice(4,-1);
       return reservemd(`<a href="${match}">${match}</a>`);
@@ -31,12 +36,15 @@ window.MDParse = function(text, custom=(t)=>{return t}) {
     .replaceAll("'", '&apos;');
   // General
   text = text
+    .replaceAll(/\`.+?\`/g, function(match){return '<code>'+reservemd(match.slice(1,-1))+'</code>'}) // Inline code
     .replaceAll(/\*\*.+?\*\*/g, function(match){return '<b>'+match.slice(2,-2)+'</b>'}) // Bold
     .replaceAll(/\*.+?\*/g, function(match){return '<i>'+match.slice(1,-1)+'</i>'}) // Italic 1
     .replaceAll(/\_\_.+?\_\_/g, function(match){return '<u>'+match.slice(2,-2)+'</u>'}) // Underline
     .replaceAll(/\_.+?\_/g, function(match){return '<i>'+match.slice(1,-1)+'</i>'}) // Italic 2
     .replaceAll(/\~\~.+?\~\~/g, function(match){return '<s>'+match.slice(2,-2)+'</s>'}) // Strikethrough
-    .replaceAll(/\`.+?\`/g, function(match){return '<code>'+match.slice(1,-1)+'</code>'}) // Inline code
+    .replaceAll(/\=\=.+?\=\=/g, function(match){return '<mark>'+match.slice(2,-2)+'</mark>'}) // Highlight
+    .replaceAll(/\~.+?\~/g, function(match){return '<sub>'+match.slice(1,-1)+'</sub>'}) // Subscript
+    .replaceAll(/\^.+?\^/g, function(match){return '<sup>'+match.slice(1,-1)+'</sup>'}) // Superscript
     .replaceAll(/^\> .+?$/gm, function(match){return '<blockquote>'+match.slice(2)+'</blockquote>'}) // Blockquote
     .replaceAll(/^(-|\*) .+?$/gm, function(match){return '<li>'+match.slice(2)+'</li>'}) // List
     .replaceAll(/^### .+?$/gm, function(match){return '<span style="font-size:110%">'+match.slice(4)+'</span>'}) // 3rd heading
